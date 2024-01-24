f2d5a1f1c8fa6444d1b0c3ee2d685668daac487d
AnuragSharda123 <anuragsharda131@gmail.com>
2024-01-24 22:18:48 +0530

    Commit

--- credma/settings.py
+++ credma/settings.py
@@ -63,8 +63,8 @@ WSGI_APPLICATION = 'credma.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'demoac0211@gmail.com'
-EMAIL_HOST_PASSWORD = 'tudqzwgzylhdmwmc'
+EMAIL_HOST_USER = 'credema.in@gmail.com'
+EMAIL_HOST_PASSWORD = 'sfetsdwjpezentlq'
 EMAIL_PORT = 587
 
 
